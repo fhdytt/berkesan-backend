d7efe28a3a582bb5e186da5820b4d9d484ca751e
fhdytt <fhddyyt@gmail.com>
2026-05-21 18:06:37 +0000

    fix:bug

---
+++ database/dummy_data_test.sql
@@ -0,0 +1,149 @@
+USE berkesan;
+
+INSERT INTO `kategori` VALUES
+(1,'Coffee','2026-05-19 15:49:53'),
+(2,'Non Coffee','2026-05-19 15:49:57'),
+(3,'Signature','2026-05-19 15:50:03');
+INSERT INTO `menu_items` VALUES
+(1,1,'Suka suka',50000.00,'data:image/jpeg;
+INSERT INTO `menu_items` VALUES
+(10,2,'Tea',10000.00,'data:image/png;
+INSERT INTO `menu_items` VALUES
+(11,2,'Lemon Tea',15000.00,'data:image/png;
+INSERT INTO `menu_items` VALUES
+(12,2,'Lychee Tea',15000.00,'data:image/png;
+INSERT INTO `menu_items` VALUES
+(13,2,'Lemon Tea',15000.00,'data:image/png;
+INSERT INTO `menu_items` VALUES
+(14,1,'V60',20000.00,'data:image/png;
+INSERT INTO `menu_items` VALUES
+(15,1,'Sunset Brew',22000.00,'data:image/jpeg;
+INSERT INTO `menu_items` VALUES
+(16,1,'Fizz Me Up',22000.00,'data:image/jpeg;
+INSERT INTO `menu_items` VALUES
+(17,2,'Vanilla Crumbs',20000.00,'data:image/png;
+INSERT INTO `menu_items` VALUES
+(18,2,'Matcha',20000.00,'data:image/png;
+INSERT INTO `menu_items` VALUES
+(20,2,'Chocolate',20000.00,'data:image/png;
+INSERT INTO `menu_items` VALUES
+(22,1,'Creamy Latte',18000.00,'data:image/jpeg;
+INSERT INTO `menu_items` VALUES
+(25,3,'Creamy Berries',22000.00,'data:image/png;
+INSERT INTO `menu_items` VALUES
+(27,3,'Dia 👍',20000.00,'data:image/jpeg;
+INSERT INTO `menu_items` VALUES
+(28,3,'Jumpa 👍',20000.00,'data:image/png;
+INSERT INTO `menu_items` VALUES
+(30,3,'Waktu 👍',20000.00,'data:image/png;
+INSERT INTO `order_items` VALUES
+(1,1,1,'Suka suka',1,50000.00,50000.00),
+(2,2,1,'Suka suka',2,50000.00,100000.00),
+(3,3,1,'Suka suka',2,50000.00,100000.00),
+(4,4,1,'Suka suka',2,50000.00,100000.00),
+(5,5,1,'Suka suka',1,50000.00,50000.00),
+(6,6,1,'Suka suka',1,50000.00,50000.00),
+(7,7,2,'Berkesan',1,25000.00,25000.00),
+(8,8,2,'Berkesan',1,35000.00,35000.00),
+(9,9,2,'Berkesan',1,35000.00,35000.00),
+(10,10,2,'Berkesan',1,35000.00,35000.00),
+(11,11,2,'Berkesan',1,35000.00,35000.00),
+(12,12,2,'Berkesan',1,35000.00,35000.00),
+(13,13,2,'Berkesan',1,35000.00,35000.00),
+(14,14,2,'Berkesan',1,35000.00,35000.00),
+(15,15,2,'Berkesan',1,35000.00,35000.00),
+(16,16,2,'Berkesan',1,35000.00,35000.00),
+(17,17,2,'Berkesan',1,35000.00,35000.00),
+(18,18,2,'Berkesan',1,35000.00,35000.00),
+(19,19,2,'Berkesan',1,35000.00,35000.00),
+(20,20,2,'Berkesan',1,35000.00,35000.00),
+(21,21,2,'Berkesan',1,35000.00,35000.00),
+(22,22,2,'Berkesan',1,35000.00,35000.00),
+(23,23,2,'Berkesan',1,35000.00,35000.00),
+(24,24,21,'Americano',1,18000.00,18000.00),
+(25,24,22,'Creamy Latte',1,18000.00,18000.00),
+(26,25,10,'Tea',1,10000.00,10000.00),
+(27,25,12,'Lychee Tea',1,15000.00,15000.00),
+(28,25,13,'Lemon Tea',1,15000.00,15000.00),
+(29,25,14,'V60',1,20000.00,20000.00),
+(30,25,15,'Sunset Brew',1,22000.00,22000.00),
+(31,25,16,'Fizz Me Up',1,22000.00,22000.00),
+(32,25,17,'Vanilla Crumbs',1,20000.00,20000.00),
+(33,25,18,'Matcha',1,20000.00,20000.00),
+(34,25,19,'Red Velvet',1,20000.00,20000.00),
+(35,25,20,'Chocolate',1,20000.00,20000.00),
+(36,25,21,'Americano',1,18000.00,18000.00),
+(37,25,22,'Creamy Latte',1,18000.00,18000.00),
+(38,25,23,'Hazelnut Latte',1,20000.00,20000.00),
+(39,25,24,'Cappucino / Cafe Latte',1,23000.00,23000.00),
+(40,25,25,'Creamy Berries',1,22000.00,22000.00),
+(41,25,27,'Dia 👍',1,20000.00,20000.00),
+(42,25,30,'Waktu 👍',1,20000.00,20000.00),
+(43,26,16,'Fizz Me Up',1,22000.00,22000.00),
+(44,27,21,'Americano',1,18000.00,18000.00),
+(45,28,21,'Americano',1,18000.00,18000.00),
+(46,29,21,'Americano',1,18000.00,18000.00),
+(47,29,24,'Cappucino / Cafe Latte',1,23000.00,23000.00),
+(48,30,16,'Fizz Me Up',1,22000.00,22000.00),
+(49,30,22,'Creamy Latte',1,18000.00,18000.00),
+(50,31,22,'Creamy Latte',1,18000.00,18000.00),
+(51,31,24,'Cappucino / Cafe Latte',1,23000.00,23000.00),
+(52,32,16,'Fizz Me Up',1,22000.00,22000.00),
+(53,33,21,'Americano',1,18000.00,18000.00),
+(54,34,21,'Americano',1,18000.00,18000.00),
+(55,35,21,'Americano',1,18000.00,18000.00),
+(56,36,21,'Americano',1,18000.00,18000.00),
+(57,37,22,'Creamy Latte',1,18000.00,18000.00),
+(58,37,24,'Cappucino / Cafe Latte',1,23000.00,23000.00),
+(59,38,21,'Americano',1,18000.00,18000.00),
+(60,38,22,'Creamy Latte',1,18000.00,18000.00),
+(61,39,25,'Creamy Berries',1,22000.00,22000.00),
+(62,39,27,'Dia 👍',1,20000.00,20000.00),
+(63,40,28,'Jumpa 👍',1,20000.00,20000.00);
+INSERT INTO `orders` VALUES
+(1,'ORD-20260519-160504488',NULL,'Fahmi',50000.00,'cash',0.00,0.00,'selesai','','2026-05-19 16:05:04','2026-05-19 16:27:05',4,NULL,'unpaid',NULL),
+(2,'ORD-20260519-160630629',NULL,'oaaware',100000.00,'qris',0.00,0.00,'dibatalkan','','2026-05-19 16:06:30','2026-05-19 17:10:38',4,NULL,'unpaid',NULL),
+(3,'ORD-20260519-160828737',NULL,'kawenr',100000.00,'qris',0.00,0.00,'dibatalkan','','2026-05-19 16:08:28','2026-05-19 17:10:40',4,NULL,'unpaid',NULL),
+(4,'ORD-20260519-160858298',NULL,'werwe',100000.00,'qris',0.00,0.00,'dibatalkan','','2026-05-19 16:08:58','2026-05-19 17:10:43',4,NULL,'unpaid',NULL),
+(5,'ORD-20260519-161418870',NULL,'Meja 20',50000.00,'qris',0.00,0.00,'selesai','','2026-05-19 16:14:18','2026-05-19 16:15:28',4,NULL,'unpaid',NULL),
+(6,'ORD-260519-165444764',NULL,'Meja 20',50000.00,'qris',0.00,0.00,'dibatalkan','','2026-05-19 16:54:44','2026-05-19 17:10:48',4,NULL,'unpaid',NULL),
+(7,'ORD-260519-170937676',NULL,'Raihan',25000.00,'cash',0.00,0.00,'selesai','','2026-05-19 17:09:37','2026-05-19 17:25:52',6,NULL,'unpaid',NULL),
+(8,'ORD-260519-173723308',NULL,'babay',35000.00,'cash',0.00,0.00,'pending','','2026-05-19 17:37:23','2026-05-19 17:37:23',6,NULL,'unpaid',NULL),
+(9,'ORD-260520-024619430',NULL,'uta',35000.00,'qris',0.00,0.00,'dibatalkan','','2026-05-20 02:46:19','2026-05-20 16:13:29',7,NULL,'unpaid',NULL),
+(10,'ORD-260520-025424600',NULL,'Meja 2',35000.00,'qris',0.00,0.00,'diproses','','2026-05-20 02:54:24','2026-05-20 17:06:34',7,NULL,'unpaid',NULL),
+(11,'ORD-260520-025508690',NULL,'Meja 2',35000.00,'cash',0.00,0.00,'diproses','','2026-05-20 02:55:08','2026-05-20 17:06:33',7,NULL,'unpaid',NULL),
+(12,'ORD-260520-030029935',NULL,'Meja 2',35000.00,'qris',0.00,0.00,'diproses','','2026-05-20 03:00:29','2026-05-20 17:06:32',7,NULL,'unpaid',NULL),
+(13,'ORD-260520-030847673',NULL,'udin',35000.00,'qris',0.00,0.00,'diproses','','2026-05-20 03:08:47','2026-05-20 17:06:30',7,NULL,'unpaid',NULL),
+(14,'ORD-260520-031217776',NULL,'ppaw',35000.00,'qris',0.00,0.00,'selesai','','2026-05-20 03:12:17','2026-05-20 03:14:51',7,NULL,'unpaid',NULL),
+(15,'ORD-260520-031416234',NULL,'ppaw',35000.00,'cash',0.00,0.00,'dibatalkan','','2026-05-20 03:14:16','2026-05-20 03:14:41',7,NULL,'unpaid',NULL),
+(16,'ORD-260520-031425840',NULL,'ppaw',35000.00,'qris',0.00,0.00,'dibatalkan','','2026-05-20 03:14:25','2026-05-20 03:14:37',7,NULL,'unpaid',NULL),
+(17,'ORD-260520-034622351',NULL,'briyn',35000.00,'cash',0.00,0.00,'selesai','yg enak ya anjinfg','2026-05-20 03:46:22','2026-05-20 03:49:09',11,NULL,'unpaid',NULL),
+(18,'ORD-260520-040040496',NULL,'aku',35000.00,'qris',0.00,0.00,'diproses','','2026-05-20 04:00:40','2026-05-20 17:06:27',7,NULL,'unpaid',NULL),
+(19,'ORD-260520-040105584',NULL,'Meja 6',35000.00,'qris',0.00,0.00,'diproses','','2026-05-20 04:01:05','2026-05-20 17:06:26',11,NULL,'unpaid',NULL),
+(20,'ORD-260520-040144200',NULL,'Meja 6',35000.00,'qris',0.00,0.00,'diproses','','2026-05-20 04:01:44','2026-05-20 17:06:24',11,NULL,'unpaid',NULL),
+(21,'ORD-260520-040409249',NULL,'aku',35000.00,'qris',0.00,0.00,'diproses','','2026-05-20 04:04:09','2026-05-20 17:06:23',7,NULL,'unpaid',NULL),
+(22,'ORD-260520-040425308',NULL,'akii',35000.00,'qris',0.00,0.00,'diproses','','2026-05-20 04:04:25','2026-05-20 17:06:22',7,NULL,'unpaid',NULL),
+(23,'ORD-260520-040450794',NULL,'Meja 6',35000.00,'qris',0.00,0.00,'diproses','','2026-05-20 04:04:50','2026-05-20 17:06:21',11,NULL,'unpaid',NULL),
+(24,'ORD-260520-151421703',NULL,'fahmi',36000.00,'qris',0.00,0.00,'selesai','','2026-05-20 15:14:21','2026-05-20 16:20:42',8,NULL,'unpaid',NULL),
+(25,'ORD-260520-152538143',NULL,'Meja 1',325000.00,'qris',0.00,0.00,'dibatalkan','','2026-05-20 15:25:38','2026-05-20 16:33:45',6,NULL,'unpaid',NULL),
+(26,'ORD-260520-154419766',NULL,'aku',22000.00,'qris',0.00,0.00,'dibatalkan','','2026-05-20 15:44:19','2026-05-20 16:33:47',16,NULL,'unpaid',NULL),
+(27,'ORD-260520-154814575',NULL,'Meja 9',18000.00,'qris',0.00,0.00,'dibatalkan','','2026-05-20 15:48:14','2026-05-20 16:33:49',14,NULL,'unpaid',NULL),
+(28,'ORD-260520-155458209',NULL,'Meja 9',18000.00,'qris',0.00,0.00,'dibatalkan','','2026-05-20 15:54:58','2026-05-20 16:33:51',14,NULL,'unpaid',NULL),
+(29,'ORD-260520-155646249',NULL,'bintang',41000.00,'qris',0.00,0.00,'diproses','','2026-05-20 15:56:46','2026-05-20 17:06:14',16,NULL,'unpaid',NULL),
+(30,'ORD-260520-155733145',NULL,'bintang',40000.00,'qris',0.00,0.00,'diproses','','2026-05-20 15:57:33','2026-05-20 17:06:12',8,NULL,'unpaid',NULL),
+(31,'ORD-260520-160430574',NULL,'nathan',41000.00,'qris',0.00,0.00,'diproses','','2026-05-20 16:04:30','2026-05-20 17:06:11',11,NULL,'unpaid',NULL),
+(32,'ORD-260520-160521756',NULL,'bintang',22000.00,'qris',0.00,0.00,'diproses','','2026-05-20 16:05:21','2026-05-20 17:06:10',11,NULL,'unpaid',NULL),
+(33,'ORD-260520-160624521',NULL,'Meja 9',18000.00,'qris',0.00,0.00,'diproses','','2026-05-20 16:06:24','2026-05-20 17:06:08',14,NULL,'unpaid',NULL),
+(34,'ORD-260520-160630570',NULL,'Meja 9',18000.00,'qris',0.00,0.00,'diproses','','2026-05-20 16:06:30','2026-05-20 17:06:07',14,NULL,'unpaid',NULL),
+(35,'ORD-260520-160812395',NULL,'test',18000.00,'qris',0.00,0.00,'selesai','','2026-05-20 16:08:12','2026-05-20 16:41:24',8,NULL,'unpaid',NULL),
+(36,'ORD-260520-160915105',NULL,'Meja 9',18000.00,'qris',0.00,0.00,'selesai','','2026-05-20 16:09:15','2026-05-20 16:35:51',14,NULL,'unpaid',NULL),
+(37,'ORD-260520-162647402',NULL,'Fahmi',41000.00,'qris',0.00,0.00,'selesai','','2026-05-20 16:26:47','2026-05-20 17:05:48',8,NULL,'unpaid',NULL),
+(38,'ORD-260520-162743150',NULL,'pei',36000.00,'cash',0.00,0.00,'diproses','','2026-05-20 16:27:43','2026-05-20 16:45:35',11,NULL,'unpaid',NULL),
+(39,'ORD-260520-163005900',NULL,'bintang',42000.00,'cash',0.00,0.00,'diproses','','2026-05-20 16:30:05','2026-05-20 17:06:04',11,NULL,'unpaid',NULL),
+(40,'ORD-260520-163047807',NULL,'bintang',20000.00,'qris',0.00,0.00,'selesai','','2026-05-20 16:30:47','2026-05-20 16:45:31',11,NULL,'unpaid',NULL);
+INSERT INTO `tables` VALUES
+(4,'20','',0,'2026-05-19 16:05:00'),
+(6,'1','data:image/png;
+INSERT INTO `users` VALUES
+(1,'admin','$2b$10$em3tOq9VWAr7f7PaHusJ.uNYSrTgx7ZrDpUqGidX.AZzpAYKGkLhi','Administrator','admin','2026-05-18 13:54:37','2026-05-18 13:54:37'),
+(2,'kasir1','$2b$10$ZA8wBm7GNERHtyrNQ2twHu5v7wyeorUlX7g3vMAJYZZ6N8SD/WoHi','Bintang','kasir','2026-05-19 08:50:33','2026-05-19 08:50:33');
