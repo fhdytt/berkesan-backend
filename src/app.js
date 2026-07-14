b1fe58647888519a5e6686b2a483adb7e6208a4c
fhdytt <fhddyyt@gmail.com>
2026-07-14 05:04:27 +0000

    fix: bug CORS

--- src/app.js
+++ src/app.js
@@ -58,7 +58,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'ngrok-skip-browser-warning'],
 }));
 
 // ============================================
